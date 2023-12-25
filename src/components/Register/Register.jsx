5c97b26fdf54b5f379eeb42cced7e03ec44686d6
freelancersifat627e <freelancersifat627@gmail.comm>
2023-12-25 21:12:33 +0600

    added show password icon

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,15 +1,18 @@
 import React, { useContext, useState } from "react";
 import Lottie from "lottie-react";
 import registerAnimation from "../../../public/json/register.json";
-import { FaGoogle } from "react-icons/fa";
+import { FaEyeSlash, FaGoogle } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProviders";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
+import "./Register.css";
 
 const Register = () => {
-
-  const [error , setError] = useState('');
-  const {createUser} = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { createUser } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,17 +20,23 @@ const Register = () => {
     const email = form.email.value;
     const password = form.password.value;
     const confirm = form.confirm.value;
-    console.log(email , password , confirm);
+    console.log(email, password, confirm);
 
-    createUser(email , password)
-    .then((result) => {
-      const loggedUser = result.user;
-      console.log(loggedUser);
-    })
-    .catch((error) => {
-      setError(error.message);
-      console.error(error.message);
-    })
+    // Password Validation
+    if (password !== confirm) {
+      return toast("Password Don't Match");
+    }
+
+    createUser(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        toast("User Register Successfully");
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.error(error.message);
+      });
   };
 
   return (
@@ -60,7 +69,7 @@ const Register = () => {
                   <label className="label">
                     <span className="label-text">Email</span>
                   </label>
-                  toast(error)
+                  <span className="text-red-500">{error}</span>
                   <input
                     type="email"
                     placeholder="Enter Your Valid Email"
@@ -73,23 +82,47 @@ const Register = () => {
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    className="input input-bordered"
-                    required
-                  />
+                  <div className="input-container">
+                    <div onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? (
+                        <IoEyeSharp className="icon" />
+                      ) : (
+                        <FaEyeSlash className="icon" />
+                      )}
+                    </div>
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Enter Your Name"
+                      name="name"
+                      className="input input-bordered"
+                      id="password"
+                      required
+                    />
+                  </div>
                   <label className="label">
                     <span className="label-text">Confirm Password</span>
                   </label>
-                  <input
-                    type="password"
-                    placeholder="Confirm password"
-                    name="confirm"
-                    className="input input-bordered"
-                    required
-                  />
+                  <div className="input-container">
+                    <div
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    >
+                      {showConfirmPassword ? (
+                        <IoEyeSharp className="icon" />
+                      ) : (
+                        <FaEyeSlash className="icon" />
+                      )}
+                    </div>
+                    <input
+                      type={showConfirmPassword ? "text" : "password"}
+                      placeholder="Confirm password"
+                      name="confirm"
+                      className="input input-bordered"
+                      id="password"
+                      required
+                    />
+                  </div>
                 </div>
                 <div className="form-control mt-6">
                   <button className="btn btn-primary text-white">
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
 import AuthProviders from "./providers/AuthProviders.jsx";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
